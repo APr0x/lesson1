85a59cd80bab3b363f99158271305b97b9a2833b
Alex <aprax1s87@gmail.com>
2024-08-05 11:50:20 +0300

    module_1_4.py
    Организация программ и методы строк

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string = input("Назовите 3 основополагающих закона робототехники ")
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
