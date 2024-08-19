5a43169dda04e9f6a9160a3d31bcbd2e2f3bc34e
Alex <aprax1s87@gmail.com>
2024-08-19 15:45:21 +0300

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -4,45 +4,24 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
-    print(calls)
-def string_ifno():
-        string1 = input('Call Your City: ')
-        string2 = input('Call Your Name: ')
-        print ((len(string1), string1.upper(), string1.lower()))
-        print((len(string2), string2.upper(), string2.lower()))
-        count_calls()
-string_ifno()
-def is_contains():
-    list_to_search = 0
-    string1 = ('Batman')
-    string1_low = string1.lower()
-    string2 = ('Wolverine')
-    string2_low = string2.lower()
-    list1 = ['Man', 'BATmAN', 'Woman']
-    list1st = str(list1)
-    list1_low = list1st.lower()
-    list2 = ['Cyclops', 'Rouge', 'Jubelee']
-    list2st = str(list2)
-    list2_low = list2st.lower()
-
-    for i in string1_low:
-        for j in list1_low:
-            if i == j:
-                list_to_search = 1
-                break
-    if list_to_search == 1:
-        print(True)
-    else:
-        print(False)
-    for i in string2_low:
-        for j in list2_low:
-            if i == j:
-                list_to_search = 1
-                break
-    if list_to_search == 1:
-        print(True)
-    else:
-        print(False)
-
 count_calls()
-is_contains()
+def string_ifno(s1):
+        a = (len(s1), s1.upper(), s1.lower())
+        return a
+count_calls()
+def is_contains(s, l):
+    list_to_search = True
+    lower_l = []
+    for i in l:
+        lower_l.append(i.lower())
+    if s.lower() in lower_l:
+        return list_to_search
+    else:
+        return False
+count_calls()
+count_calls()
+print(string_ifno(s1= 'Ave Dominus Nox'))
+print(string_ifno(s1='Big floppa Cat'))
+print(is_contains(s="Batman", l=['Wonan', 'Man', 'BATman']))
+print(is_contains(s='Wolverine', l=['Jubelee', 'Beast', 'Professor X']))
+print(calls)
