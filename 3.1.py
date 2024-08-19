c1a92fdfdd7e813e114ac18aa3cb94e39629bb80
Alex <aprax1s87@gmail.com>
2024-08-19 12:27:06 +0300

    Create 3.1.py

---
+++ 3.1.py
@@ -0,0 +1,9 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls = str(input(''))
+    def string_ifno():
+        a = calls
+        b = tuple(len(calls), calls.upper(calls), calls.lower(calls))
