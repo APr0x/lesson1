c460491c061656b0b7155e790417ab51a1017e68
Alex <aprax1s87@gmail.com>
2024-08-19 13:10:39 +0300

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -6,9 +6,18 @@ def count_calls():
     calls += 1
     print(calls)
 def string_ifno():
-        string1 = 'Based Sigma Chad'
+        string1 = input('Call Your City: ')
+        string2 = input('Call Your Name: ')
         print ((len(string1), string1.upper(), string1.lower()))
+        print((len(string2), string2.upper(), string2.lower()))
+        count_calls()
 string_ifno()
 def is_contains():
-
+    string1 = ('Batman', ['Man', 'BATmAN', 'Woman'])
+    string2 = ('Wolverine', ['Cyclops', 'Rouge', 'Jubelee'])
+    print(string1)
+    print(string2)
+    list_to_search = True
+    else:
+False
 count_calls()
