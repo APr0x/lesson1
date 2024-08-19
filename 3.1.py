7c94e0c700482f5d434afabc4641cd2e154e2733
Alex <aprax1s87@gmail.com>
2024-08-19 17:39:09 +0300

    Update 3.1.py

--- 3.1.py
+++ 3.1.py
@@ -4,22 +4,21 @@ calls = 0
 def count_calls():
     global calls
     calls += 1
-count_calls()
 def string_ifno(s1):
-        a = (len(s1), s1.upper(), s1.lower())
-        return a
-count_calls()
+    count_calls()
+    a = (len(s1), s1.upper(), s1.lower())
+    return a
 def is_contains(s, l):
-    list_to_search = True
-    lower_l = []
-    for i in l:
-        lower_l.append(i.lower())
-    if s.lower() in lower_l:
-        return list_to_search
-    else:
-        return False
-count_calls()
-count_calls()
+        count_calls()
+        list_to_search = True
+        lower_l = []
+        for i in l:
+            lower_l.append(i.lower())
+        if s.lower() in lower_l:
+            return list_to_search
+        else:
+            return False
+
 print(string_ifno(s1= 'Ave Dominus Nox'))
 print(string_ifno(s1='Big floppa Cat'))
 print(is_contains(s="Batman", l=['Wonan', 'Man', 'BATman']))
