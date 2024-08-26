3bb7aa5a8591f2e3c5bc04c272155bc2f4b91da6
Alex <aprax1s87@gmail.com>
2024-08-26 15:15:59 +0300

    Update 3_2.py

--- 3_2.py
+++ 3_2.py
@@ -1,12 +1,40 @@
-def send_email(message, recipeint, *, sender='university.help@gmail.com'):
-    if '@' or '.com' or '.ru' or '.net' not in sender:
-        if '@' or '.com' or '.ru' or '.net' not in recipeint:
-            print('Невозможно отправить письмо с адреса на адрес')
-    if sender == recipeint:
-        print('Нельзя отправить сообщение самому себе')
-    if sender =='university.help@gmail.com':
-        print('Письмо успешно отправлено с адреса  на адрес .')
+def send_email(message, recipient, *args, sender='university.help@gmail.com'):
+    sobaka = '@'
+    end = ('.com', '.net', '.ru')
+    flag = False
+    if sobaka in recipient:
+        for _ in end:
+            if _ in recipient:
+                flag = True
+    if flag is False:
+        error = ('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient, '.')
+        return error
+    sobaka = '@'
+    end = ('.com', '.net', '.ru')
+    flag = False
+    if sobaka in recipient:
+        for _ in end:
+            if _ in recipient:
+                flag = True
+    if flag is False:
+        error = ('Невозможно отправить письмо с адреса ', sender, ' на адрес ', recipient, '.')
+        return error
+    if recipient == sender:
+        error1 = ('Нельзя отправить письмо самому себе!')
+        return error1
+    sender = 'university.help@gmail.com'
+    check_sender = 'university.help@gmail.com'
+    flag = True
+    if check_sender in sender:
+        if _ in sender:
+            if _ in check_sender:
+                flag = False
+    if not flag:
+        service_message = 'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса ', sender, 'на адрес', recipient
+        return service_message
     else:
-        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>.')
+        success = ('Письмо успешно отправлено с адреса ', sender, ' на адрес ', recipient,'.')
+        return success
 
-print(send_email('Привет, друг!', 'friend@mail.com'))
+
+print(send_email('Hello, World', 'bobr_kurwa@mail.ru'))
